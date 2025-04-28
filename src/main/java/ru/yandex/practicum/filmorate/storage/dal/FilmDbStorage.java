836383a3e1afd3b402d93d584c98ea22c93314ad
Integra <Integra4220@yandex.ru>
2025-04-29 01:03:25 +0300

    fix checkstyle2

--- src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dal/FilmDbStorage.java
@@ -20,7 +20,6 @@ import ru.yandex.practicum.filmorate.storage.dal.mappers.MpaRowMapper;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
